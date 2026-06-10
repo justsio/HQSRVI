89cdc4750b8988d460b686908c3caa6546317ac3
v0agent <it+v0agent@vercel.com>
2026-06-10 07:35:06 +0000

    feat: update theme color to #1db488
    Changed the theme color from #15803d to #1db488 for a new design update.

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
-        <meta name="theme-color" content="#15803d" />
+        <meta name="theme-color" content="#1db488" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="font-sans">
